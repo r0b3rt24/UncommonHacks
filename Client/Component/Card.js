16a30bcf166bcc6d27a18c7a5816f5415fdcca53
Yuheng Chen <ccyyhh97@hotmail.com>
2019-02-16 21:43:51 -0600

    card refined

--- Client/Component/Card.js
+++ Client/Component/Card.js
@@ -4,6 +4,9 @@ import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 export default class Card extends Component{
     render(){
         const styles = StyleSheet.create({
+            cardShadow:{
+
+            },
             card: {
                 flex: 0,
                 // width: 0,
@@ -13,13 +16,11 @@ export default class Card extends Component{
                 alignItems: 'center',     
                 borderRadius: 10,
                 backgroundColor: '#fff',
+                overflow: 'hidden',
             },
             img: {
                 height: 300, 
-                width: 300,
-                overflow: 'hidden',
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 10
+                width: 300,    
             },
             textWrapper: {
                 height: 50,
@@ -35,7 +36,8 @@ export default class Card extends Component{
 
         });
         return(
-            <View style={styles.card} shadowColor={'grey'} shadowRadius={3} shadowOpacity={.75} shadowOffset={{ height: 2, width: 1 }}>
+            <View style={styles.cardShadow} shadowColor={'grey'} shadowRadius={3} shadowOpacity={.75} shadowOffset={{ height: 2, width: 1 }}>
+            <View style={styles.card} >
                 <Image
                     style={styles.img}
                     source={{uri: 'https://static1.squarespace.com/static/56227557e4b0007a00065273/58cf33b029687fc957dcb83e/58cf38bdf7e0ab911f7a1123/1489975789154/yuri-shwedoff-return-internet.jpg?format=2500w'}}
@@ -45,6 +47,7 @@ export default class Card extends Component{
                     <Text style={styles.words}>Restaurant name</Text>
                     <Text style={styles.words}>$ $ $</Text>
                 </View>
+            </View>
             </View>
         );          
     }  
