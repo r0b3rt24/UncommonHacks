a5c9481420ab1ad85175ba51202bd1900f2993da
Yuheng Chen <ccyyhh97@hotmail.com>
2019-02-16 20:44:59 -0600

    card

--- Client/App.js
+++ Client/App.js
@@ -8,7 +8,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Card from './Component/Card'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,10 +23,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+       <Card/>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+      
     );
   }
 }
@@ -33,18 +36,19 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    // fontSize: 120,
+    // textAlign: 'center',
+    // margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    // fontSize: 30
+    // textAlign: 'center',
+    // color: '#333333',
+    // marginBottom: 5,
   },
 });
